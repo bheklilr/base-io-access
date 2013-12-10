30c122c3f58a02c0dba6ae78acb45a56b768b0bf
Aaron Stevens <bheklilr2@gmail.com>
2013-12-09 22:04:31 -0600

    Implemented Access.System.Timeout

--- Access/System/Timeout.hs
+++ Access/System/Timeout.hs
@@ -1,1 +1,16 @@
-module Access.System.Timeout () where
+module Access.System.Timeout
+    ( module System.Timeout
+
+    , TimeoutAccess(..)
+    ) where
+
+
+import System.Timeout
+
+
+class Monad io => TimeoutAccess io where
+    timeout' :: Int -> IO a -> io (Maybe a)
+
+
+instance TimeoutAccess IO where
+    timeout' = timeout
