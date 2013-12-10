f859cfa0197a70bacfdce77c2c26e3392c7c0030
Aaron Stevens <bheklilr2@gmail.com>
2013-12-09 22:05:45 -0600

    Implemented Access.System.CPUTime

--- Access/System/CPUTime.hs
+++ Access/System/CPUTime.hs
@@ -1,1 +1,16 @@
-module Access.System.CPUTime () where
+module Access.System.CPUTime
+    ( module System.CPUTime
+
+    , CPUTimeAccess(..)
+    ) where
+
+
+import System.CPUTime
+
+
+class Monad io => CPUTimeAccess io where
+    getCPUTime' :: io Integer
+
+
+instance CPUTimeAccess IO where
+    getCPUTime' = getCPUTime
