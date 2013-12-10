1e3320857c314bc96941a8068e8abd6a06873a2f
Aaron Stevens <bheklilr2@gmail.com>
2013-12-09 22:13:35 -0600

    Implemented Access.System.Mem.Weak

--- Access/System/Mem/Weak.hs
+++ Access/System/Mem/Weak.hs
@@ -1,1 +1,20 @@
-module Access.System.Mem.Weak () where
+module Access.System.Mem.Weak
+    ( module System.Mem.Weak
+
+    , WeakMemAccess(..)
+    ) where
+
+
+import System.Mem.Weak
+
+
+class Monad io => WeakMemAccess io where
+    mkWeak'     :: k -> v -> Maybe (IO ()) -> io (Weak v)
+    deRefWeak'  :: Weak v -> io (Maybe v)
+    finalize'   :: Weak v -> io ()
+
+
+instance WeakMemAccess IO where
+    mkWeak'    = mkWeak
+    deRefWeak' = deRefWeak
+    finalize'  = finalize
