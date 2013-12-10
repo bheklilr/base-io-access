5af8f220bbc504a6afdabf8af8090f915b5101f0
Aaron Stevens <bheklilr2@gmail.com>
2013-12-09 22:02:57 -0600

    Implemented Access.System.Mem

--- Access/System/Mem.hs
+++ Access/System/Mem.hs
@@ -1,1 +1,16 @@
-module Access.System.Mem () where
+module Access.System.Mem
+    ( module System.Mem
+
+    , MemAccess(..)
+    ) where
+
+
+import System.Mem
+
+
+class Monad io => MemAccess io where
+    performGC' :: io ()
+
+
+instance MemAccess IO where
+    performGC' = performGC
