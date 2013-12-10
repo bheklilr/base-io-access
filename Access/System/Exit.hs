0b2b49a8e72546250529341b744c645e36ab8e17
Aaron Stevens <bheklilr2@gmail.com>
2013-12-09 21:58:12 -0600

    Implemented Access.System.Exit

--- Access/System/Exit.hs
+++ Access/System/Exit.hs
@@ -1,1 +1,20 @@
-module Access.System.Exit () where
+module Access.System.Exit
+    ( module System.Exit
+
+    , ExitAccess(..)
+    ) where
+
+
+import System.Exit
+
+
+class Monad io => ExitAccess io where
+    exitWith'       :: ExitCode -> io a
+    exitFailure'    :: io a
+    exitSuccess'    :: io a
+
+
+instance ExitAccess IO where
+    exitWith'    = exitWith
+    exitFailure' = exitFailure
+    exitSuccess' = exitSuccess
