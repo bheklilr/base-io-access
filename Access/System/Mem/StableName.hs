ea0624e3a63201bd088fb43777994377fd84003b
Aaron Stevens <bheklilr2@gmail.com>
2013-12-09 22:15:00 -0600

    Implemented Access.System.Mem.StableName

--- Access/System/Mem/StableName.hs
+++ Access/System/Mem/StableName.hs
@@ -1,1 +1,16 @@
-module Access.System.Mem.StableName () where
+module Access.System.Mem.StableName
+    ( module System.Mem.StableName
+
+    , StableNameAccess(..)
+    ) where
+
+
+import System.Mem.StableName
+
+
+class Monad io => StableNameAccess io where
+    makeStableName' :: a -> io (StableName a)
+
+
+instance StableNameAccess IO where
+    makeStableName' = makeStableName
