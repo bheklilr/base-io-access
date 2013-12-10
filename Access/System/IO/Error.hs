03ea40d057bdc05347a8f256a0c7347dde009bbd
Aaron Stevens <bheklilr2@gmail.com>
2013-12-09 22:10:07 -0600

    Implemented Access.System.IO.Error

--- Access/System/IO/Error.hs
+++ Access/System/IO/Error.hs
@@ -1,1 +1,22 @@
-module Access.System.IO.Error () where
+module Access.System.IO.Error
+    ( module System.IO.Error
+
+    , IOErrorAccess(..)
+    ) where
+
+
+import System.IO.Error
+
+
+class Monad io => IOErrorAccess io where
+    ioError'        :: IOError -> io a
+    catchIOError'   :: io a -> (IOError -> io a) -> io a
+    tryIOError'     :: io a -> io (Either IOError a)
+    modifyIOError'  :: (IOError -> IOError) -> io a -> io a
+
+
+instance IOErrorAccess IO where
+    ioError'       = ioError
+    catchIOError'  = catchIOError
+    tryIOError'    = tryIOError
+    modifyIOError' = modifyIOError
