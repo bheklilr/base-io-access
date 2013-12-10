85765125f4791726b44d9c2539d99f863bfb74a0
Aaron Stevens <bheklilr2@gmail.com>
2013-12-09 22:01:26 -0600

    Implemented Access.System.Environment

--- Access/System/Environment.hs
+++ Access/System/Environment.hs
@@ -1,1 +1,30 @@
-module Access.System.Environment () where
+module Access.System.Environment
+    ( module System.Environment
+
+    , EnvironmentAccess(..)
+    ) where
+
+
+import System.Environment
+
+
+class Monad io => EnvironmentAccess io where
+    getArgs'            :: io [String]
+    getProgName'        :: io String
+    getExecutablePath'  :: io FilePath
+    getEnv'             :: String -> io String
+    lookupEnv'          :: String -> io (Maybe String)
+    withArgs'           :: [String] -> IO a -> io a
+    withProgName'       :: String -> IO a -> io a
+    getEnvironment'     :: io [(String, String)]
+
+
+instance EnvironmentAccess IO where
+    getArgs'           = getArgs
+    getProgName'       = getProgName
+    getExecutablePath' = getExecutablePath
+    getEnv'            = getEnv
+    lookupEnv'         = lookupEnv
+    withArgs'          = withArgs
+    withProgName'      = withProgName
+    getEnvironment'    = getEnvironment
