469bc34d1e675b1196aac27e2bfc931bdc6403f4
Aaron Stevens <bheklilr2@gmail.com>
2013-12-28 15:47:33 -0600

    Added documentation to Access.Core

--- Access/Core.hs
+++ Access/Core.hs
@@ -1,3 +1,12 @@
+--------------------------------------------------------------------------------
+-- |
+-- Module       :   Access.Core
+-- Copyright    :   (c) Aaron Stevens, 2014
+-- License      :   GPL2
+--
+-- Maintainer   :   bheklilr2@gmail.com
+--------------------------------------------------------------------------------
+
 module Access.Core
     ( Access
     ) where
@@ -7,6 +16,8 @@ import Data.Typeable.Internal (Typeable1)
 import Control.Applicative    (Applicative)
 import Control.Monad.Fix      (MonadFix)
 
+-- | The 'Access' type class.  It belongs to several of the same type classes
+-- as 'IO'.  Notably, it is a 'Monad', a 'Functor', and an 'Applicative'.
 class ( Monad io
       , Functor io
       , Typeable1 io
